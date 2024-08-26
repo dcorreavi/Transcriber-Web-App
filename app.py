fc93ff1bc78b1e890f5747f71198a555daee4650
Daniel <dcorreavi@gmail.com>
2024-08-26 18:31:28 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
